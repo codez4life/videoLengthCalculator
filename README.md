1add81adf23af45d1bd5c7ca438c4401d2d9c6ea
codez4life <johnquarcoopome.jnr@gmail.com>
2020-01-31 11:40:05 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# videoLengthCalculator
+
+A desktop software that helps you see the supposed length of a video
+
+#Electron
---
+++ index.js
@@ -0,0 +1,16 @@
+const electron = require("electron");
+const { app, BrowserWindow,ipcMain } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+
+   let mainWindow;
+
+app.on("ready", () => {
+   mainWindow = new BrowserWindow({})
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+ipcMain.on('video:submit',(event,path)=>{
+  ffmpeg.ffprobe(path,(err,metadata) => {
+    //console.log(`Video duration is ${metadata.format.duration}`)
+   Mainwindow.webContents.send('video:metadata',metadata.format.duration);
+  })
+})
